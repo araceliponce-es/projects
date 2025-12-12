c846c554f323e0bc2b24a0f97504f719b82ccf71
diegorodriguezalonso13-cpu <diegorodriguezalonso13>
2025-12-12 09:36:01 +0100

    Aver

--- Ahorcado/Ahorcado parte1/src/ahorcado/parte1/HiddenWord.java
+++ Ahorcado/Ahorcado parte1/src/ahorcado/parte1/HiddenWord.java
@@ -8,8 +8,33 @@ package ahorcado.parte1;
  *
  * @author Araceli,Diego,Óscar
  */
-public class AhorcadoParte1 {
-    private int[] characters=new int[16];
+public class HiddenWord {
+
+    private char[] characters = new char[16];
+    private boolean[] hits = new boolean[characters.length];
+
+    public char[] getCharacters() {
+        return characters;
+    }
+
+    public void setCharacters(char[] characters) {
+        this.characters = characters;
+    }
+
+    public boolean[] getHits() {
+        return hits;
+    }
+
+    public void setHits(boolean[] hits) {
+        this.hits = hits;
+    }
+
+    public HiddenWord(String palabra) {
+        characters = palabra.toCharArray();
+        for (int i = 0; i < hits.length; i++) {
+            hits[i] = false;
+        }
+    }
 
     /**
      * @param args the command line arguments
@@ -17,5 +42,25 @@ public class AhorcadoParte1 {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
+    public boolean checkChar(char c) {
+        for (int i = 0; i < hits.length; i++) {
+            if (c == characters[i]) {
+                hits[i] = true;
+            }
+        }
+        return false;
+    }
+
+    public String show() {
+        for (int i = 0; i < hits.length; i++) {
+            if (hits[i] = true) {
+                return "" + characters[i];
+            } else {
+                return "-";
+            }
+        }
+        return "yes";
+    }
+
 }
