3594c5c3f5c87c62e6eb748533f744fbf6a4e8e2
diegorodriguezalonso13-cpu <diegorodriguezalonso13>
2025-12-15 18:26:32 +0100

    Medio método

--- Ahorcado/Ahorcado parte1/src/ahorcado/parte1/HangMan.java
+++ Ahorcado/Ahorcado parte1/src/ahorcado/parte1/HangMan.java
@@ -4,10 +4,28 @@
  */
 package ahorcado.parte1;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author daw1al11
  */
 public class HangMan {
+    public static final int MAX_FAILS=6;
+    HiddenWord hiddenWord=new HiddenWord("Guacamole");
+    private ArrayList<Character> fails=new ArrayList();
+
+    public HangMan(String palabra) {
+        hiddenWord=new HiddenWord(palabra);
+    }
+    
+    public ArrayList<Character> getFails(){
+        return fails;
+    }
+    public String getStringFails(){
+        String fallos=" ";
+        return fallos;
+    }
+    
     
 }
