c775a572aa100267c2104b06a1f6d4631bdb9a3e
diegorodriguezalonso13-cpu <diegorodriguezalonso13>
2025-12-16 09:49:59 +0100

    Class 2

--- Ahorcado/Ahorcado parte1/src/ahorcado/parte1/HangMan.java
+++ Ahorcado/Ahorcado parte1/src/ahorcado/parte1/HangMan.java
@@ -11,21 +11,42 @@ import java.util.ArrayList;
  * @author daw1al11
  */
 public class HangMan {
-    public static final int MAX_FAILS=6;
-    HiddenWord hiddenWord=new HiddenWord("Guacamole");
-    private ArrayList<Character> fails=new ArrayList();
+
+    public static final int MAX_FAILS = 6;
+    HiddenWord hiddenWord = new HiddenWord("");
+    private ArrayList<Character> fails = new ArrayList();
 
     public HangMan(String palabra) {
-        hiddenWord=new HiddenWord(palabra);
+        hiddenWord = new HiddenWord(palabra);
     }
-    
-    public ArrayList<Character> getFails(){
+
+    public ArrayList<Character> getFails() {
         return fails;
     }
-    public String getStringFails(){
-        String fallos=" ";
-        return fallos;
+
+    public String getStringFails() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < fails.size(); i++) {
+            sb.append(fails.get(i)).append(" ");
+        }
+        return sb.toString();
     }
-    
-    
+
+    public String showHidenWord() {
+        return hiddenWord.show();
+    }
+
+    public String showFullWord() {
+        return hiddenWord.showFullWord();
+    }
+
+    public void tryChar(char caracter) {
+        if (hiddenWord.checkChar(caracter)==false) {
+            fails.add(caracter);
+        }else{
+            
+        }
+
+    }
+
 }
