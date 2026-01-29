a9558f2a1b13eac888518ca4c37b5c9fbe599a6a
diegorodriguezalonso13-cpu <diegorodriguezalonso13>
2026-01-29 11:30:07 +0100

    Comentarios1

--- Ahorcado/Ahorcado parte1/src/ahorcado/parte1/HiddenWord.java
+++ Ahorcado/Ahorcado parte1/src/ahorcado/parte1/HiddenWord.java
@@ -7,31 +7,71 @@ package ahorcado.parte1;
 import java.util.Scanner;
 
 /**
+ * Clase que implementa una palabra a adivinar y se encargará de comprobar que
+ * las letras introducidas están o no en la palabra, de mostrar la palabra sin
+ * adivinar y de mostrar la palabra completa una vez termine el juego
  *
  * @author Araceli,Diego,Óscar
  */
 public class HiddenWord {
 
+    /**
+     * Array de caracteres que almacenará los caracteres de la palabra secreta
+     */
     private char[] characters;
+
+    /**
+     * Array de booleanos que almacenará true en la posición donde se acierten
+     * los caracters de la palabra y false cuando no
+     */
     private boolean[] hits;
 
+    /**
+     * Obtiene los caracteres de la palabra para el array
+     *
+     * @return
+     */
     public char[] getCharacters() {
         return characters;
     }
 
+    /**
+     * Establece los caracteres de la palabra para el array
+     *
+     * @param characters
+     */
     public void setCharacters(char[] characters) {
         this.characters = characters;
     }
 
+    /**
+     * Obtiene los aciertos y fallos al introducir una letra para intentar
+     * adivinar la palabra
+     *
+     * @return Booleano que indica si aparece la letra o no en la palabra
+     */
     public boolean[] getHits() {
         return hits;
     }
 
+    /**
+     * Establece los aciertos y fallos al introducir una letra para intentar
+     * adivinar la palabra
+     *
+     * @param hits
+     */
     public void setHits(boolean[] hits) {
         this.hits = hits;
     }
 
+    /**
+     * Constructor que recibe la palabra secreta como parámetro, la almacena en
+     * el array de caracteres e inicializa todos los hits a false
+     *
+     * @param palabra
+     */
     public HiddenWord(String palabra) {
+        //Convierte la String de la palabra en caracteres
         characters = palabra.toCharArray();
         hits = new boolean[characters.length];
         for (int i = 0; i < hits.length; i++) {
@@ -39,6 +79,13 @@ public class HiddenWord {
         }
     }
 
+    /**
+     * Método que comprueba un caracter recibido y marca las posiciones donde
+     * aparece en la palabra como true en hits
+     *
+     * @param caracter
+     * @return Booleano que indica si el caracter aparece o no
+     */
     public boolean checkChar(char caracter) {
         boolean charFound = false;
         for (int i = 0; i < characters.length; i++) {
@@ -51,8 +98,16 @@ public class HiddenWord {
         return charFound;
     }
 
+    /**
+     * Método que muestra la palabra incompleta, sustituyendo letras sin acertar
+     * por guiones
+     *
+     * @return La palabra secreta con guiones en lugar de las letras no
+     * acertadas
+     */
     public String show() {
         StringBuilder sb = new StringBuilder();
+        //Bucle que añade al sb el caracter si se acertó y un guión si no
         for (int i = 0; i < characters.length; i++) {
             if (hits[i]) {
                 sb.append(characters[i]);
@@ -62,24 +117,26 @@ public class HiddenWord {
         }
         return sb.toString();
     }
-//    public String showBis() {
-//
-//        for (int i = 0; i < characters.length; i++) {
-//            if (hits[i]) {
-//                System.out.print(characters[i]);
-//            } else {
-//                System.out.print("-");
-//            }
-//        }
-//        return "";
-//    }
 
+    /**
+     * Método que muestra la palabra secreta al completo, se usará cuando
+     * termine el juego
+     *
+     * @return Un string formado con todos los caracteres de la palabra secreta
+     */
     public String showFullWord() {
         return new String(characters);
     }
 
+    /**
+     * Método que indica si la palabra es completamente visible porque se
+     * acertaron todos sus caracteres
+     *
+     * @return Booleano que indica si el usuario ganó la partida
+     */
     public boolean isVisible() {
         boolean acertada = true;
+        //Si hay algún hit false no se adivinó la palabra, el bucle busca algún hit que sea false
         for (int i = 0; i < hits.length && acertada; i++) {
             if (!hits[i]) {
                 acertada = false;
--- Ahorcado/Ahorcado parte1/src/ahorcado/parte1/WordGenerator.java
+++ Ahorcado/Ahorcado parte1/src/ahorcado/parte1/WordGenerator.java
@@ -5,21 +5,32 @@
 package ahorcado.parte1;
 
 /**
+ * Clase que "genera" la palabra escogiendo una aleatoria dentro de un array de
+ * palabras
  *
  * @author Araceli,Diego,Óscar
  */
 public class WordGenerator {
 
+    /**
+     * Array de strings que almacena las posibles palabras secretas
+     */
     private String[] seleccionPalabras = new String[5];
 
+    /**
+     * Método que guarda unas palabras en el array, genera un número aleatorio
+     * dentro de la longitud del array y devuelve la palabra escogida
+     *
+     * @return La palabra secrerta que hay que adivinar
+     */
     public String generateWord() {
-        seleccionPalabras[0]="Guacamole";
-        seleccionPalabras[1]="Aguacate";
-        seleccionPalabras[2]="Cilantro";
-        seleccionPalabras[3]="Jalapeño";
-        seleccionPalabras[4]="Cebolla";
+        seleccionPalabras[0] = "Guacamole";
+        seleccionPalabras[1] = "Aguacate";
+        seleccionPalabras[2] = "Cilantro";
+        seleccionPalabras[3] = "Jalapeño";
+        seleccionPalabras[4] = "Cebolla";
         int escogida = new java.util.Random().nextInt(5);
-        String palabra=seleccionPalabras[escogida];
+        String palabra = seleccionPalabras[escogida];
         return palabra;
     }
 
