463d16700f7f06754269ce20dc6ab457062ccbe4
diegorodriguezalonso13-cpu <diegorodriguezalonso13>
2026-01-30 09:47:20 +0100

    Fin ahorcado1

--- Ahorcado/Ahorcado parte1/src/ahorcado/parte1/HangMan.java
+++ Ahorcado/Ahorcado parte1/src/ahorcado/parte1/HangMan.java
@@ -7,23 +7,55 @@ package ahorcado.parte1;
 import java.util.ArrayList;
 
 /**
+ * Clase que implementa el estado de una partida de ahorcado, hará referencia a
+ * la clase de la palabra oculta y almacenará y mostrará las letras probadas que
+ * no estén en la palabra
  *
  * @author Araceli,Diego,Óscar
  */
 public class HangMan {
 
+    /**
+     * Constante que define el máximo de fallos que puede tener el usuario antes
+     * de perder
+     */
     public static final int MAX_FAILS = 6;
+
+    /**
+     * Objeto que referencia a la clase HiddenWord para usar sus métodos
+     */
     private HiddenWord hiddenWord;
+
+    /**
+     * ArrayList que almacena las letras que se probaron y que no están en la
+     * palabra secreta
+     */
     private ArrayList<Character> fails = new ArrayList();
 
+    /**
+     * Constructor que recibe un string con la palabra secreta e inicializa el
+     * hiddenWord con ella
+     *
+     * @param palabra
+     */
     public HangMan(String palabra) {
         hiddenWord = new HiddenWord(palabra);
     }
 
+    /**
+     * Método que obtiene las letras falladas del ArrayList de fallos
+     *
+     * @return Caracteres que no están en la palabra
+     */
     public ArrayList<Character> getFails() {
         return fails;
     }
 
+    /**
+     * Método que muestra por pantalla las letras del ArrayList
+     *
+     * @return Un string con todas las letras falladas separadas con un espacio
+     */
     public String getStringFails() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < fails.size(); i++) {
@@ -32,24 +64,54 @@ public class HangMan {
         return sb.toString();
     }
 
+    /**
+     * Método que llama al método show de hiddenWord para devolver la palabra
+     * secreta con guiones en los caracteres no acertados
+     *
+     * @return La palabra secreta con guiones en los caracteres no acertados
+     */
     public String showHidenWord() {
         return hiddenWord.show();
     }
 
+    /**
+     * Método que llama al método showFullWord para devolver la palabra secreta
+     * al completo
+     *
+     * @return La palara secreta con todas las letras
+     */
     public String showFullWord() {
         return hiddenWord.showFullWord();
     }
 
+    /**
+     * Método que recibe un caracter y comprueba si pertenece a la palabra
+     * secreta, si lo hace se marcarán todas sus apariciones como acertadas y si
+     * no, se añade al ArrayList de fallos
+     *
+     * @param caracter
+     */
     public void tryChar(char caracter) {
         if (!hiddenWord.checkChar(caracter)) {
             fails.add(caracter);
-        } 
+        }
     }
 
+    /**
+     * Método que comprueba si se terminó la partida porque se adivinó la
+     * palabra o porque se llegó al límite de fallos
+     *
+     * @return Booleano que indica si se terminó la partida o no
+     */
     public boolean isGameOver() {
         return hiddenWord.isVisible() || maxFailsExceeded();
     }
 
+    /**
+     * Método que comprueba si se llegó al límite de fallos o no
+     *
+     * @return Booleano que indica si se llegó al límite de fallos o no
+     */
     public boolean maxFailsExceeded() {
         boolean excedido;
         excedido = fails.size() == MAX_FAILS;
--- Ahorcado/Ahorcado parte1/src/ahorcado/parte1/MenuGenerator.java
+++ Ahorcado/Ahorcado parte1/src/ahorcado/parte1/MenuGenerator.java
@@ -7,11 +7,15 @@ package ahorcado.parte1;
 import java.util.Scanner;
 
 /**
+ * Clase que muestra los menús del juego y recoge los datos del usuario
  *
  * @author Araceli,Diego,Óscar
  */
 public class MenuGenerator {
 
+    /**
+     * Objeto de tipo hangMan que gestionará el funcionamiento de la partida
+     */
     private HangMan hangMan;
 
     /**
@@ -22,36 +26,51 @@ public class MenuGenerator {
         do {
             menuGenerator.hangMan = new HangMan(menuGenerator.showInitMenu());
             menuGenerator.showGameMenu();
-            if(menuGenerator.hangMan.isGameOver() && menuGenerator.hangMan.maxFailsExceeded()){
-                System.out.println("Perdiste, la palabra era: "+menuGenerator.hangMan.showFullWord());
-            }else if(menuGenerator.hangMan.isGameOver()){
-                System.out.println("Ganaste, la palabra era: "+menuGenerator.hangMan.showFullWord());
+            if (menuGenerator.hangMan.isGameOver() && menuGenerator.hangMan.maxFailsExceeded()) {
+                System.out.println("Perdiste, la palabra era: " + menuGenerator.hangMan.showFullWord());
+            } else if (menuGenerator.hangMan.isGameOver()) {
+                System.out.println("Ganaste, la palabra era: " + menuGenerator.hangMan.showFullWord());
             }
         } while (!menuGenerator.showExitMenu());
-        
 
     }
 
+    /**
+     * Método que "muestra" el menú para generar la palabra secreta, actualmente
+     * sólo la genera y la devuelve
+     *
+     * @return La palabra secreta
+     */
     private String showInitMenu() {
         WordGenerator palabraSecr = new WordGenerator();
         return palabraSecr.generateWord();
     }
 
+    /**
+     * Método que muestra el menú del juego en el que se pedirán las letras y se
+     * mostrarán los fallos y aciertos, se detendrá si se termina el juego
+     */
     private void showGameMenu() {
         Scanner scan = new Scanner(System.in);
         while (!hangMan.isGameOver()) {
-            
+
             System.out.println("¿Qué letra estará en esta palabra misteriosa?:");
-            System.out.println("");
+            System.out.println();
             char c = scan.next().charAt(0);
             hangMan.tryChar(c);
             System.out.println();
             System.out.println(hangMan.showHidenWord());
-            System.out.println("Letras falladas: "+hangMan.getStringFails());
+            System.out.println("Letras falladas: " + hangMan.getStringFails());
         }
 
     }
 
+    /**
+     * Método que muestra el menú de fin de partida y pregunta al usuario si
+     * quiere jugar otra vez
+     *
+     * @return Booleano que indica si el usuario quiere jugar otra vez o no
+     */
     private boolean showExitMenu() {
         if (hangMan.isGameOver()) {
             System.out.println("¿Quieres jugar otra vez?(s/n)");
